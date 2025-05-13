f26bcc2d8589b9b71702503b62627865a299ff8a
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-05-13 19:31:29 +0530

    mysql railwayy setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,39 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//   }
+// );
+
+// module.exports = sequelize;
+
+
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME,   // Database name (school_db)
+  process.env.DB_USER,   // Username (root)
+  process.env.DB_PASS,   // Password (Mayank)
   {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    host: process.env.DB_HOST,  
+    dialect: 'mysql',            
+    port: 3306,                  
+    dialectOptions: {
+      ssl: {
+        require: true,           
+        rejectUnauthorized: false 
+      }
+    }
   }
 );
+
 
 module.exports = sequelize;
